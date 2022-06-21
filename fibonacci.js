bb6f2552638fd53566b6ed00d6cd01e4f4509289
Leonardo B. Hurovich <105162659+leobh76@users.noreply.github.com>
2022-06-21 12:48:41 +0300

    change in the function

--- fibonacci.js
+++ fibonacci.js
@@ -2,11 +2,11 @@ const fibInput = document.querySelector("#form12");
 const button = document.querySelector("#button");
 const fibNumber = document.querySelector("#fib-number");
 
-const fibFunction = (n) => {
+function fibFunction(n) {
   if (n < 2) {
     return n;
   } else return fibFunction(n - 1) + fibFunction(n - 2);
-};
+}
 
 button.addEventListener("click", () => {
   fibNumber.innerText = fibFunction(parseInt(fibInput.value));
