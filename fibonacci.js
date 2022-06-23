53bfa5bf7c87677da969d36bd3164593e5c16174
Leonardo B. Hurovich <105162659+leobh76@users.noreply.github.com>
2022-06-23 12:02:54 +0300

    some more changes - not final

--- fibonacci.js
+++ fibonacci.js
@@ -1,18 +1,27 @@
 const fibInput = document.getElementById("form12");
 const button = document.getElementById("button");
 const fibNumber = document.querySelector("#fib-number");
+const spinner = document.getElementById("spinner");
+const error42 = document.getElementById("error-42");
+const error50 = document.getElementById("error-50");
 
 function getFibNumber() {
   const index = fibInput.value;
 
-  fetch(`${"http://localhost:5050/fibonacci/"}${index}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const result = data.result;
-      fibNumber.innerText = result;
-    });
+  if (index === 42) {
+    error42.classList.remove("d-none");
+  } else if (index > 50) {
+    error50.classList.remove("d-none");
+  } else if (index <= 50) {
+    fetch(`${"http://localhost:5050/fibonacci/"}${index}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const result = data.result;
+        fibNumber.innerText = result;
+      });
+  }
 }
 
 button.addEventListener("click", getFibNumber);
