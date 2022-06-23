1264584a044ae8e7a7305ba4185c21838ccf8f34
Leonardo B. Hurovich <105162659+leobh76@users.noreply.github.com>
2022-06-23 17:54:58 +0300

    final changes for milestone-5

--- fibonacci.js
+++ fibonacci.js
@@ -2,20 +2,35 @@ const fibInput = document.getElementById("form12");
 const button = document.getElementById("button");
 const fibNumber = document.querySelector("#fib-number");
 const spinner = document.getElementById("spinner");
-const error42 = document.getElementById("error-42");
 const error50 = document.getElementById("error-50");
+const error42 = document.getElementById("error-42");
 
 function getFibNumber() {
   const index = fibInput.value;
 
-  if (index === 42) {
-    error42.classList.remove("d-none");
-  } else if (index > 50) {
-    error50.classList.remove("d-none");
-  } else if (index <= 50) {
+  if (index > 50) {
+    /* if input is more than 50 */
+    spinner.classList.remove("d-none");
+    setTimeout(() => {
+      spinner.classList.add("d-none");
+      error50.classList.remove("d-none");
+    }, 500);
+  } else {
+    /* if it is 50 or less */
+    spinner.classList.remove("d-none");
+    setTimeout(() => {
+      spinner.classList.add("d-none");
+    }, 500);
     fetch(`${"http://localhost:5050/fibonacci/"}${index}`)
       .then((response) => {
-        return response.json();
+        if (!response.ok) {
+          response.text().then((error) => {
+            console.log("Server error:" + error);
+            fibNumber.innerText = error;
+          });
+        } else {
+          return response.json();
+        }
       })
       .then((data) => {
         const result = data.result;
@@ -24,4 +39,13 @@ function getFibNumber() {
   }
 }
 
+// event listeners for the button
+fibInput.addEventListener("focus", clearInput);
 button.addEventListener("click", getFibNumber);
+
+// function to clear input when focusing it again
+function clearInput() {
+  fibInput.value = null;
+  fibNumber.innerText = null;
+  error50.classList.add("d-none");
+}
