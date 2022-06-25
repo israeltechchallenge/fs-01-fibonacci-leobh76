dc64d8da3f0af522ba09e4084cbc1fc939dbb365
Leonardo B. HUrovich <105162659+leobh76@users.noreply.github.com>
2022-06-26 00:21:14 +0300

    milestone-6 final version (small aesthetic changes)

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,4 @@
+// Variables:
 const fibInput = document.getElementById("form12");
 const button = document.getElementById("button");
 const fibNumber = document.querySelector("#fib-number");
@@ -6,15 +7,14 @@ const error50 = document.getElementById("error-50");
 const resultLog = document.getElementById("result-line");
 const resultsSpinner = document.getElementById("spinner-results");
 
-window.addEventListener("load", getResultsList);
-
+// Function for the results list:
 function getResultsList() {
   resultsSpinner.classList.remove("d-none");
   fetch("http://localhost:5050/getFibonacciResults")
-    .then((response) => response.json())
-    .then((data) => {
-      resultsSpinner.classList.add("d-none");
-      let list = data.results;
+  .then((response) => response.json())
+  .then((data) => {
+    resultsSpinner.classList.add("d-none");
+    let list = data.results;
       for (item of list) {
         let newLine = document.createElement("div");
         newLine.innerHTML = `The Fibonnaci of <b>${item.number}</b> is <b>${
@@ -24,28 +24,29 @@ function getResultsList() {
         newLine.className = "mb-2 text-decoration-underline";
       }
     });
-}
-
-function getFibNumber() {
-  const index = fibInput.value;
-
-  if (index > 50) {
-    /* if input is more than 50 */
+  }
+  
+  // Function to calculate the Fibonacci number:
+  function getFibNumber() {
+    const index = fibInput.value;
+    
+    if (index > 50) {
+    // if input is more than 50
     spinner.classList.remove("d-none");
     setTimeout(() => {
       spinner.classList.add("d-none");
       error50.classList.remove("d-none");
     }, 500);
   } else {
-    /* if it is 50 or less */
+    // if it is 50 or less
     spinner.classList.remove("d-none");
     setTimeout(() => {
       spinner.classList.add("d-none");
     }, 500);
     fetch(`${"http://localhost:5050/fibonacci/"}${index}`)
-      .then((response) => {
-        if (!response.ok) {
-          response.text().then((error) => {
+    .then((response) => {
+      if (!response.ok) {
+        response.text().then((error) => {
             console.log("Server error:" + error);
             fibNumber.innerText = `Server Error: ${error}`;
             fibNumber.classList.add("text-danger");
@@ -62,12 +63,15 @@ function getFibNumber() {
   }
 }
 
-fibInput.addEventListener("focus", clearInput);
-button.addEventListener("click", getFibNumber);
-
-// clear input when focused
+// Function to clear input when focused:
 function clearInput() {
   fibInput.value = null;
   fibNumber.innerText = null;
   error50.classList.add("d-none");
 }
+
+// Event listeners:
+window.addEventListener("load", getResultsList);
+fibInput.addEventListener("focus", clearInput);
+button.addEventListener("click", getFibNumber);
+
