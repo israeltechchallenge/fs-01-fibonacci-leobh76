00e25d974aee47d2f855ebcb65814bcdbabb7fa5
Leonardo B. HUrovich <105162659+leobh76@users.noreply.github.com>
2022-06-26 21:23:45 +0300

    milestone-7 (almost finished, minor fix pending)

--- fibonacci.js
+++ fibonacci.js
@@ -1,29 +1,66 @@
 // Variables:
 const fibInput = document.getElementById("form12");
-const button = document.getElementById("button");
+const buttonLocal = document.getElementById("button-local");
+const buttonServer = document.getElementById("button-server");
 const fibNumber = document.querySelector("#fib-number");
 const spinner = document.getElementById("spinner");
 const error50 = document.getElementById("error-50");
 const resultLog = document.getElementById("result-line");
 const resultsSpinner = document.getElementById("spinner-results");
+const checkBox = document.getElementById("flexCheckDefault");
+const error00 = document.getElementById("error-00");
 
-/*
+// Event listeners:
+window.addEventListener("load", getResultsList);
+
+fibInput.addEventListener("focus", clearInput);
+
+buttonServer.addEventListener("click", getFibNumber);
+
+buttonLocal.addEventListener("click", () => {
+  spinner.classList.remove("d-none");
+  setTimeout(() => {
+    spinner.classList.add("d-none");
+    fibNumber.innerText = fibFunction(parseInt(fibInput.value));
+  }, 200);
+});
+
+// Swapping between local and server buttons
+checkBox.addEventListener("change", () => {
+  if (checkBox.checked) {
+    // hide the local button
+    buttonLocal.classList.add("d-none");
+    buttonServer.classList.remove("d-none");
+  } else if (!checkBox.checked) {
+    // hide the server button
+    buttonLocal.classList.remove("d-none");
+    buttonServer.classList.add("d-none");
+  }
+});
+
 // Function to calculate the Fibonacci number LOCALLY:
 function fibFunction(n) {
-  if (n < 2) {
+  if (n < 0) {
+    error00.classList.remove("d-none");
+    fibNumber.classList.add("d-none");
+  } else if (n < 2) {
+    error00.classList.add("d-none");
     return n;
-  } else return fibFunction(n - 1) + fibFunction(n - 2);
+  } else if (n > 50) {
+    error00.classList.add("d-none");
+    error50.classList.remove("d-none");
+    fibNumber.classList.add("d-none");
+  } else {
+    error00.classList.add("d-none");
+    fibNumber.classList.remove("d-none");
+    return fibFunction(n - 1) + fibFunction(n - 2);
+  }
 }
 
-button.addEventListener("click", () => {
-  fibNumber.innerText = fibFunction(parseInt(fibInput.value));
-});
-*/
-
-// Function to calculate the Fibonacci number:
-  function getFibNumber() {
-    const index = fibInput.value;
-    if (index > 50) {
+// Function to calculate the Fibonacci number ON THE SERVER:
+function getFibNumber() {
+  const index = fibInput.value;
+  if (index > 50) {
     // if input is more than 50
     spinner.classList.remove("d-none");
     setTimeout(() => {
@@ -34,24 +71,25 @@ button.addEventListener("click", () => {
     // if it is 50 or less
     spinner.classList.remove("d-none");
     fetch(`${"http://localhost:5050/fibonacci/"}${index}`)
-    .then((response) => {
-      if (!response.ok) {
-        response.text().then((error) => {
-          console.log("Server error:" + error);
-          fibNumber.innerText = `Server Error: ${error}`;
-          fibNumber.classList.add("text-danger");
-          spinner.classList.add("d-none");
-        });
-      } else {
-        return response.json();
-      }
-    })
-    .then((data) => {
-      fibNumber.classList.remove("text-danger");
-      const result = data.result;
-      fibNumber.innerText = result;
-      spinner.classList.add("d-none");
-    });
+      .then((response) => {
+        if (!response.ok) {
+          response.text().then((error) => {
+            console.log("Server error:" + error);
+            fibNumber.innerText = `Server Error: ${error}`;
+            fibNumber.classList.add("text-danger");
+            fibNumber.classList.remove("d-none");
+            spinner.classList.add("d-none");
+          });
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        fibNumber.classList.remove("text-danger");
+        const result = data.result;
+        fibNumber.innerText = result;
+        spinner.classList.add("d-none");
+      });
   }
 }
 
@@ -60,12 +98,8 @@ function clearInput() {
   fibInput.value = null;
   fibNumber.innerText = null;
   error50.classList.add("d-none");
+  error00.classList.add("d-none");
 }
-
-// Event listeners:
-window.addEventListener("load", getResultsList);
-fibInput.addEventListener("focus", clearInput);
-button.addEventListener("click", getFibNumber);
 
 // Function for the results list:
 function getResultsList() {
